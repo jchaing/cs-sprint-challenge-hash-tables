1341b8ac97d56938334b45453415c266292a055b
john <john@spinningplates.org>
2020-09-13 19:37:22 -0700

    Completed ex3.py and tested

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -4,6 +4,34 @@ def intersection(arrays):
     """
     # Your code here
 
+    # array dictionary
+    array_dict = {}
+
+    # result list
+    result = []
+
+    # loop through the arrays
+    for array in arrays:
+        # loop through numbers in array
+        for num in array:
+            if num not in array_dict:
+                # tally 1 as value if not in dict
+                array_dict[num] = 1
+            else:
+                # if num is in dict plus add 1
+                array_dict[num] += 1
+
+    length = len(arrays)
+
+    # loop through dict as a list
+    for item in list(array_dict.items()):
+        # if item value is equal length
+        # then all arrays have same item number
+        if item[1] == length:
+            # add item number to list
+            result.append(item[0])
+
+    # return list of same item numbers
     return result
 
 
